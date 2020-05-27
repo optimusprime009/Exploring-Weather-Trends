d8ba704d25006be8b97a11ae4b89f87c66af9311
Himanshu Tripathi <himanshut3344@gmail.com>
2020-05-27 09:37:35 +0530

    Update SqlQuery.sql

--- SqlQuery.sql
+++ SqlQuery.sql
@@ -1,8 +1,4 @@
-/*
-This file has the commands that i  used to download the CSV files
-from an online database using SQL.
-PS : because my city was not in the city_list, I have downloaded the information for "Cairo" instead.
-*/
+
 SELECT * FROM global_data;
 
 SELECT * FROM city_data
